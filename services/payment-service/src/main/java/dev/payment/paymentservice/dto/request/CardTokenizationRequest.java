17208f12699534be2e5e5e903ab68c4bb924f3fc
Umesh Lohot <umeshlohot@gmail.com>
2026-04-15 15:38:13 +0530

    fix: add JsonProperty mappings for tokenize request
    - Add proper snake_case to camelCase mappings in CardTokenizationRequest
    - Add separate route for tokenize endpoint in gateway
    - Disable gateway-trust in docker (already disabled in local)

--- services/payment-service/src/main/java/dev/payment/paymentservice/dto/request/CardTokenizationRequest.java
+++ services/payment-service/src/main/java/dev/payment/paymentservice/dto/request/CardTokenizationRequest.java
@@ -1,16 +1,19 @@
 package dev.payment.paymentservice.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 
 public record CardTokenizationRequest(
     @NotBlank(message = "Card number is required")
+    @JsonProperty("card_number")
     String cardNumber,
     
     @NotBlank(message = "Expiry month is required")
+    @JsonProperty("expiry_month")
     String expiryMonth,
     
     @NotBlank(message = "Expiry year is required")
+    @JsonProperty("expiry_year")
     String expiryYear,
     
     @NotBlank(message = "CVV is required")
